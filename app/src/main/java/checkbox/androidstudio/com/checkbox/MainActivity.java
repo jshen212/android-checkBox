59bf7db7b60c86a3c523d524db2dc27671f2468f
simplejeff <jeffshen86@gmail.com>
2016-09-22 02:25:54 -0700

    [feature] created functioning checkboxes with text view display

---
+++ app/src/main/java/checkbox/androidstudio/com/checkbox/MainActivity.java
@@ -0,0 +1,47 @@
+package checkbox.androidstudio.com.checkbox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CheckBox checkBoxDog;
+    private CheckBox checkBoxCat;
+    private CheckBox checkBoxDragon;
+
+    private Button showButton;
+
+    private TextView showTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        showTextView = (TextView) findViewById(R.id.textShowId);
+        checkBoxDog = (CheckBox) findViewById(R.id.checkBoxDog);
+        checkBoxCat = (CheckBox) findViewById(R.id.checkBoxCat);
+        checkBoxDragon = (CheckBox) findViewById(R.id.checkBoxDragon);
+
+        showButton = (Button) findViewById(R.id.showButtonId);
+
+        showButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                StringBuilder stringBuilder = new StringBuilder();
+
+                stringBuilder.append(checkBoxDog.getText().toString() + " status is: " + checkBoxDog.isChecked() + "\n");
+
+                stringBuilder.append(checkBoxCat.getText().toString() + " status is: " + checkBoxCat.isChecked() + "\n");
+
+                stringBuilder.append(checkBoxDragon.getText().toString() + " status is: " + checkBoxDragon.isChecked() + "\n");
+
+                showTextView.setText(stringBuilder);
+            }
+        });
+    }
+}
